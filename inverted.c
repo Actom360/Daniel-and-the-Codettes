8008d9ac6501fe1940179d3872c831ad95cbec19
Aynel <aynell__@hotmail.com>
2017-05-26 17:27:21 +1000

    some new files

---
+++ inverted.c
@@ -0,0 +1,12 @@
+/*
+reads data from a given collection of pages in collection.txt
+and generates an "inverted index" that provides a list (set) of
+urls for every word in a given collection of pages. 
+
+You need to "normalise" words by removing leading and trailing spaces
+and converting all characters to lowercases before inserting words in
+your index. In each list (set), duplicate urls are not allowed.
+Your program should output this "inverted index" to a file named invertedIndex.txt.
+The list should be alphabetically ordered, using ascending order.
+*/
+
--- searchTfIdf.c
+++ searchTfIdf.c
@@ -1,1 +1,7 @@
 // searchTfIdf.c
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+
